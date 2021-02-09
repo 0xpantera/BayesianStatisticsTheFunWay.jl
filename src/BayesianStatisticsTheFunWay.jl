c7a07e5f5c024ad2584dab1aff26b6c1d0e49b1b
Francososa <franky.sosa@gmail.com>
2021-02-08 23:40:52 -0600

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.10

---
+++ src/BayesianStatisticsTheFunWay.jl
@@ -0,0 +1,5 @@
+module BayesianStatisticsTheFunWay
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using BayesianStatisticsTheFunWay
+using Test
+
+@testset "BayesianStatisticsTheFunWay.jl" begin
+    # Write your tests here.
+end
